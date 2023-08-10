bad175826e74b46efb7ba2c1396b67b2ef6781ea
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2023-08-10 17:30:51 -0400

    login

--- scrapeJob.js
+++ scrapeJob.js
@@ -11,10 +11,10 @@ function delay(time) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	//Retrieve amount of new job
-	await page.goto("https://www.linkedin.com/jobs/search?keywords=Software%2BEngineer&location=New%2BYork%2BCity%2BMetropolitan%2BArea&geoId=90000070&f_TPR=r172800&f_PP=102571732&position=23&pageNum=0");
+	await page.goto('https://www.linkedin.com/jobs/search?keywords=Software%2BEngineer&location=New%2BYork%2BCity%2BMetropolitan%2BArea&geoId=90000070&f_TPR=r172800&f_PP=102571732&position=23&pageNum=0');
 
 	const amountOfJob = await page.evaluate(() => {
-        const baseCard = document.querySelector(".base-serp-page").querySelector(".base-serp-page__content").querySelector("#main-content").querySelector(".results-context-header").querySelector(".results-context-header__context").querySelector("span").innerText;
+        const baseCard = document.querySelector('.base-serp-page').querySelector('.base-serp-page__content').querySelector('#main-content').querySelector('.results-context-header').querySelector('.results-context-header__context').querySelector('span').innerText;
         return baseCard;
     })
     await console.log(amountOfJob);
@@ -29,19 +29,19 @@ function delay(time) {
 		await delay(5000);
 		const quotes = await page.evaluate(() => {
 
-			const baseList = document.querySelectorAll("li");
+			const baseList = document.querySelectorAll('li');
 		
 			return Array.from(baseList).flatMap((base) => {
 				try {
-					const baseCard = base.querySelector(".base-card");
+					const baseCard = base.querySelector('.base-card');
 					const link = baseCard.querySelector('a').getAttribute('href');
-					const information = baseCard.querySelector(".base-search-card__info");
-					const jobTitle = information.querySelector(".base-search-card__title").innerText;
-					const company = information.querySelector(".base-search-card__subtitle").querySelector("a").innerText;
+					const information = baseCard.querySelector('.base-search-card__info');
+					const jobTitle = information.querySelector('.base-search-card__title').innerText;
+					const company = information.querySelector('.base-search-card__subtitle').querySelector('a').innerText;
 		
-					const datetime = information.querySelector(".base-search-card__metadata").querySelector('time').getAttribute('datetime');
-					const checkFor = ["senior", "staff", "sr", "founding", "data", "machine", "ai", "java", "c++", "cloud", "it", 
-										"net","lead","manager"]
+					const datetime = information.querySelector('.base-search-card__metadata').querySelector('time').getAttribute('datetime');
+					const checkFor = ['senior', 'staff', 'sr', 'founding', 'data', 'machine', 'ai', 'java', 'c++', 'cloud', 'it', 
+										'net','lead','manager']
 		
 					const hasSome = checkFor.some(word => jobTitle.toLowerCase().includes(word))
 					if (hasSome){
--- scrapeRecruiter.js
+++ scrapeRecruiter.js
@@ -1,12 +1,28 @@
 const puppeteer = require('puppeteer');
-
-
+const {getUsername, getPassword} = require('./envVariable.js');
+const proxy = '2.56.119.93';
+function delay(time) {
+	return new Promise(function(resolve) { 
+		setTimeout(resolve, time)
+	});
+}
 (async () => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({headless: false});
 	const page = await browser.newPage();
-    
-	await page.goto("https://www.linkedin.com/jobs/search?keywords=Software%2BEngineer&location=New%2BYork%2BCity%2BMetropolitan%2BArea&geoId=90000070&f_TPR=r172800&f_PP=102571732&position=23&pageNum=0");
+	await page.goto('https://www.linkedin.com');
+    await delay(4000);
+    const usernameInput = await page.$("#session_key");
 
+    await usernameInput.type(getUsername());
 
+    const passwordInput = await page.$("#session_password");
+
+    await passwordInput.type(getPassword());
+    const btn = await page.$('[data-id="sign-in-form__submit-btn"]');
+    await btn.click();
+    await delay(30000);
+    await page.screenshot({
+        path: 'shot.jpg'
+    });
 	await browser.close();
 })();
