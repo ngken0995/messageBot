9a0d16a3b83e628664dd81a8493dc1f062a0b18c
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2023-08-10 14:44:07 -0400

    test git ignore

--- envVariable.js
+++ envVariable.js
@@ -1,4 +1,14 @@
 
+
+const getUsername = () => {
+    return 'ngken0995@gmai.com';
+}
+
+
+const getPassword = () => {
+    return ''
+}
+
 const getJobSearchUrl = () => {
     return 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?distance=25&f_PP=102571732%2C104361728&f_TPR=r432000&geoId=90000070&keywords=Software%2BEngineer&start=0';
 }
